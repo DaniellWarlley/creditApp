622952d9473b3e67bc19ddbe2201c3c3c3ce4850
DaniellWarlley <warlleydaniell1@gmail.com>
2026-04-23 21:28:33 -0300

    da

--- server/index.js
+++ server/index.js
@@ -5,11 +5,7 @@ import dotenv from 'dotenv'
 import connectDb from './dataBase/connectDb.js'
 
 const app = express()
-<<<<<<< HEAD
 const PORT = process.env.PORT || 3333
-=======
-const PORT = process.env.PORT || 3000
->>>>>>> 0e3a7e7e5b0ee4a9cc37330471b2d8eeb989605b
 
 dotenv.config()
 app.use(express.json())
@@ -21,18 +17,9 @@ app.get('/', (req, res) => {
 })
 
 connectDb()
-<<<<<<< HEAD
     .then(() => console.log("Banco conectado"))
     .catch(err => console.error("Erro no banco:", err));
 
 app.listen(PORT, () => {
     console.log("rodando servidor");
 });
-=======
-  .then(() => console.log("Banco conectado"))
-  .catch(err => console.error("Erro no banco:", err));
-
-app.listen(PORT, () => {
-  console.log("rodando servidor");
-});
->>>>>>> 0e3a7e7e5b0ee4a9cc37330471b2d8eeb989605b
