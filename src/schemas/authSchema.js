fbe01f29a8dced0a720353691f28dc8ee6466de1
DaniellWarlley <warlleydaniell1@gmail.com>
2026-04-26 11:47:59 -0300

    push forced

--- src/schemas/authSchema.js
+++
@@ -1,8 +0,0 @@
-import z from 'zod'
-
-export const authSchema = z.object({
-    name: z.string().min(1, 'Nome obrigatório').optional(),
-    email: z.string().email('Email invalido'),
-    passWord: z.string().min(8, 'Senha com menos de 8 caracteres')
-})
-
